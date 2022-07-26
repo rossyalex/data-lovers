4b30c506f4b267401adb841b647442c9077ff48b
Misdely Morales <misdely.m@gmail.com>
2022-07-25 22:11:02 -0400

    cambios en footer, mneú (responsive), y estilos de página

--- src/main.js
+++ src/main.js
@@ -2,3 +2,10 @@
 import data from './data/harrypotter/data.js';
 
 console.log(data.characters);
+
+
+const menuIcon= document.querySelector("#menu-icon");
+menuIcon.addEventListener("click", function(){
+    const menu=document.querySelector(".nav-links");
+    menu.classList.toggle("open");
+})
