34a1b034c86809e10f24a4beedafb1798b2004e4
Misdely Morales <misdely.m@gmail.com>
2022-08-05 19:14:54 -0400

    Filtro pocion o hechizo

--- src/js/potion.js
+++ src/js/potion.js
@@ -15,8 +15,13 @@ export function showPotions() {
   printPotions(data.potions);
 }
 
+//Mostrar Spell
+export function showSpells() {
+  printSpells(data.spells);
+}
+
 //Función mostrar potions
-function printPotion(potions) {
+function printPotions(potions) {
   clear();
   const potionList = document.getElementById("potions-card");
   potions.map((potions) => {
@@ -41,26 +46,34 @@ function printPotion(potions) {
 }
 
 //Función mostrar hechizos
-function printPotion(potions) {
+function printSpells(spells) {
   clear();
-  const potionList = document.getElementById("potions-card");
-  potions.map((potions) => {
-    const newPotion = `<div class="col-3">
+  const spellList = document.getElementById("potions-card");
+  spells.map((spells) => {
+    const newSpell = `<div class="col-3">
       <div class="scene">
         <div class="card">
             <div class="card_face card-front">
-                <img src="img/spellsandpotion/spells.jpg" class="img-spell" alt="spell">
+                <img src="img/spellsandpotion/potions.jpg" class="img-spell" alt="spell">
             </div>
             <div class="card_face card-back">
             <h3>Name</h3>  
-            <p>${potions.name}</p>
+            <p>${spells.name}</p>
+            <h3>Other Name</h3>  
+            <p>${spells.other_name}</p>
+            <h3>Pronunciation</h3>  
+            <p>${spells.pronunciation}</p>
+            <h3>Spell Type</h3>  
+            <p>${spells.spell_type}</p>
             <h3>Description</h3>  
-            <p>${potions.description}</p>
+            <p>${spells.description}</p>
+            <h3>Etymology</h3>  
+            <p>${spells.etymology}</p>
             </div>
         </div>
       </div>
     </div>`;
-    potionList.insertAdjacentHTML("beforeend", newPotion);
+    spellList.insertAdjacentHTML("beforeend", newSpell);
   });
   addEffectToBooks();
 }
@@ -72,11 +85,31 @@ function clear() {
 
 //Funcion para sort alfabeticamente
 
-export function sortPotions(order) {
-  const potionSorted = data.potions.sort((a, b) => {
-    if (order === "asc") return a.name.localeCompare(b.name);
-    else if (order === "desc") return b.name.localeCompare(a.name);
-  });
+export function filterByPotionsSepells (){
+  const selectPagePotion = document.getElementById("selectPotionSpell");
+selectPagePotion.addEventListener("change", (e) => {
 
-  printBooks(potionSorted);
+  if (e.target.value === "potions") {
+    showPotions();
+  } else if (e.target.value === "spells") {
+    showSpells();
+  }
+});
 }
+
+//NO FUNCIONA
+export function sortPotions(order) {
+  const sortByType = document.getElementById("sortPotionSpell").value;
+  const filterType = filterByPotionsSepells(sortByType);
+    if (order === "asc"){
+      filterType.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+    }else if (order === "desc"){
+      filterType.sort((a, b) => {
+        return b.name.localeCompare(a.name);
+    });
+  };
+  printPotions(sortByType);
+}
+
